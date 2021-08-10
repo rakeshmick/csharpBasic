25bad1f16eea2afca6510e4f84a1ed25a2f6e073
Rakesh Prabhakaran <rakesh.1985march@gmail.com>
2021-08-11 08:11:42 +1000

    initial

---
+++ Employee.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Basics
+{
+    public class Employee
+    {
+        public Employee()
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://horse.industryconnect.io/Account/Login";
+            Console.WriteLine("Title of the application opened"+driver.Title);
+            driver.Close();
+            driver.Quit();
+            Console.ReadLine();
+        }
+    }
+}
